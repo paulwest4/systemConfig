09c79b93d9439774dc86258688f3b4d500c14c00
Paul West <pwest@palantir.com>
2018-03-04 18:22:06 -0500

    manage symbolic hotkeys

---
+++ configs/setup.sh
@@ -0,0 +1,5 @@
+cd ~/configs
+git config core.worktree "../../"
+git reset --hard origin/master
+[ -f ~/Library/Preferences/com.apple.symbolichotkeys.plist ] && cp ~/Library/Preferences/com.apple.symbolichotkeys.plist ~/Library/Preferences/com.apple.symbolichotkeys.plist.bak
+ln -sf com.apple.symbolichotkeys.plist ~/Library/Preferences/com.apple.symbolichotkeys.plist
